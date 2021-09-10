5fb7cfb532fc553fc151bb947bf73b24d8975a7d
Nisrine Hamdouni <Nisrine@iMac-de-Nisrine.local>
2021-09-10 10:45:09 +0200

    Fin Section 5

--- src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
+++ src/main/java/com/mycompany/invoise/invoiseweb/controller/InvoiceControllerWeb.java
@@ -3,17 +3,20 @@ package com.mycompany.invoise.invoiseweb.controller;
 import com.mycompany.invoise.core.controller.InvoiceControllerInterface;
 import com.mycompany.invoise.core.entity.Invoice;
 import com.mycompany.invoise.core.service.InvoiceServiceInterface;
+import com.mycompany.invoise.invoiseweb.form.InvoiceForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
 @RequestMapping("/invoice")
-public class InvoiceControllerWeb implements InvoiceControllerInterface {
+public class InvoiceControllerWeb {
 
     @Autowired
     private InvoiceServiceInterface invoiceService;
@@ -27,7 +30,13 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
     }
 
     @PostMapping("")
-    public String createInvoice(Invoice invoice) {
+    public String createInvoice(@Valid @ModelAttribute InvoiceForm invoiceForm, BindingResult results) {
+        if(results.hasErrors()) {
+            return "invoise-create-form";
+        }
+        Invoice invoice = new Invoice();
+        invoice.setCustomerName(invoiceForm.getCustomerName());
+        invoice.setOrderNumber(invoiceForm.getOrderNumber());
         invoiceService.createInvoice(invoice);
         return "invoise-created";
     }
@@ -47,7 +56,7 @@ public class InvoiceControllerWeb implements InvoiceControllerInterface {
     }
 
     @GetMapping("/create-form")
-    public String displayInvoiceCreateForm(Invoice invoice) {
+    public String displayInvoiceCreateForm(InvoiceForm invoice) {
         return "invoise-create-form";
     }
 
